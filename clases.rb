63fd93007dbea51fdcc31e91a224a1654744425d
Elian Rojas <impresosgosen@gmail.com>
2021-02-22 13:59:20 -0600

    Herencia, Polimorfismo y Modulos

---
+++ clases.rb
@@ -0,0 +1,74 @@
+class Animal
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+module Habilidades #swim
+  module Volador
+    def volar
+      'Estoy volandooooo!'
+    end
+    def aterrizar
+        'Estoy cansado de volar, voy a aterrizar'
+    end
+  end
+    module Nadador
+    def nadar
+      'Estoy nadando!'
+    end
+    def sumergir
+      'glu glub glub glu'
+    end
+  end
+    module Caminante
+    def caminar
+      'Puedo caminar!'
+    end
+  end
+end
+module Alimentacion
+  module Herbivoro
+    def comer
+      'Puedo comer plantas!'
+    end
+  end
+  module Carnivoro
+    def comer
+      'Puedo comer carne!'
+    end
+  end
+end
+class Bird < Animal
+end
+class Mammal < Animal
+end
+class Insect < Animal
+end
+class Penguin < Bird
+  include Habilidades::Caminante, Alimentacion::Carnivoro
+end
+class Pigeon < Bird #paloma
+  include Habilidades::Volador, Habilidades::Caminante, Alimentacion::Herbivoro
+end
+class Duck < Bird
+  include Habilidades::Nadador, Habilidades::Caminante, Habilidades::Volador
+end
+class Dog < Mammal
+  include Habilidades::Caminante, Habilidades::Nadador, Alimentacion::Carnivoro
+end
+class Cat < Mammal
+  include Habilidades::Caminante, Alimentacion::Carnivoro
+end
+class Cow < Mammal
+  include Habilidades::Caminante, Alimentacion::Herbivoro
+end
+class Flay < Insect #mosca
+  include Habilidades::Volador, Habilidades::Caminante, Alimentacion::Carnivoro
+end
+class Butterfly < Insect
+  include Habilidades::Volador, Alimentacion::Herbivoro
+end
+class Bee < Insect
+  include Habilidades::Volador, Alimentacion::Herbivoro
+end
--- modules.rb
+++ modules.rb
@@ -1,36 +1,35 @@
-module Habilidades
+module Habilidades #swim
   module Volador
     def volar
-    'Estoy volandooooo!'
+      'Estoy volandooooo!'
     end
     def aterrizar
-    'Estoy cansado de volar, voy a aterrizar'
+        'Estoy cansado de volar, voy a aterrizar'
     end
   end
-  module Nadador
+    module Nadador
     def nadar
-    'Estoy nadando!'
+      'Estoy nadando!'
     end
     def sumergir
-    'glu glub glub glu'
+      'glu glub glub glu'
     end
   end
-  module Caminante
+    module Caminante
     def caminar
-    'Puedo caminar!'
+      'Puedo caminar!'
     end
   end
 end
-
 module Alimentacion
   module Herbivoro
     def comer
-    'Puedo comer plantas!'
+      'Puedo comer plantas!'
     end
   end
   module Carnivoro
     def comer
-    'Puedo comer carne!'
+      'Puedo comer carne!'
     end
   end
 end
